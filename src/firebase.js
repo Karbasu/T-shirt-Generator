6cdf36d2c9ef5f6460222f6521dfa7efd1ecd86b
Karbasappa B <karbasappabembulge@gmail.com>
2023-09-27 22:22:54 +0530

    firebase config file added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB22FDHuAA2Vk9b3B_6XxduPIscynL4UbE",
+  authDomain: "t-shirt-designerr.firebaseapp.com",
+  projectId: "t-shirt-designerr",
+  storageBucket: "t-shirt-designerr.appspot.com",
+  messagingSenderId: "927623079170",
+  appId: "1:927623079170:web:848134be410c8527a3ce1b",
+};
